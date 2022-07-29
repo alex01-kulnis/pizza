7c8cf19ffdf09c50fa8cbc5e83aaf63c5e0f6212
Alex Kulnis <kulnis71@gmail.com>
2022-07-29 15:54:11 +0300

    add ts to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElem = document.getElementById('root');
 
-root.render(
-  <React.StrictMode>
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+
+  root.render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+    </BrowserRouter>,
+  );
+}
