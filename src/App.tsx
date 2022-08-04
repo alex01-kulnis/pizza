3f3a17ecb5f13cbb46a56c1dd5b52365b5455de6
Alex Kulnis <kulnis71@gmail.com>
2022-08-04 12:32:42 +0300

    work with lazi loading and webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
+//import NotFound from './pages/NotFound';
+//import Cart from './pages/Cart';
+//import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout.jsx';
 
 import './scss/app.scss';
+
+import Loading from './components/Loading';
+
+const Cart = lazy(() => import(/*webpackChunkName: "Cart" */ './pages/Cart'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const FullPizza = lazy(() => import('./pages/FullPizza'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<Loading />}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
